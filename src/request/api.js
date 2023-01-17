1af08572590ba35dc167f250215c4f7e6ea4ae48
FanOne <294350394@qq.com>
2023-01-17 16:05:56 +0800

    feat:api task

--- src/request/api.js
+++ src/request/api.js
@@ -3,3 +3,11 @@ import request from "./request";
 export const RegisterApi =(params)=> request.post('/user/register',params)
 
 export const LoginApi =(params)=> request.post('/user/login',params)
+
+export const TaskListApi = (params) => request.get("/tasks", { params });
+
+export const TaskCreateApi = (params) => request.post("/task", params );
+
+export const TaskUpdateApi = (params) => request.put(`/task/${params.id}`, params );
+
+export const TaskDeleteApi = (params) => request.delete(`/task/${params.id}`);
